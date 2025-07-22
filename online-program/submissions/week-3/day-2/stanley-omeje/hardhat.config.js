2849227e268ad017983fcf941046d9ef82886ff2
stanley omeje <stanzealot@gmail.com>
2025-07-22 22:39:09 +0100

    updated

--- online-program/submissions/week-3/day-2/stanley-omeje/hardhat.config.js
+++ online-program/submissions/week-3/day-2/stanley-omeje/hardhat.config.js
@@ -12,16 +12,12 @@ module.exports = {
       url: "https://rpc.sepolia-api.lisk.com",
       accounts: vars.has("PRIVATE_KEY") ? [vars.get("PRIVATE_KEY")] : [],
       chainId: 4202,
-    },
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${vars.get("ALCHEMY_API_KEY")}`,
-      accounts: vars.has("PRIVATE_KEY") ? [vars.get("PRIVATE_KEY")] : [],
-    },
+    }
+    
   },
   etherscan: {
     apiKey: {
       liskSepolia: "abc", 
-      sepolia: ETHERSCAN_API_KEY,
     },
     customChains: [
       {
