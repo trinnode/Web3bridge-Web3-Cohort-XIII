20ddb2f79c7307a4ec973f226f77d45d3da67dd4
george chisom <georgechipaul@gmail.com>
2025-07-22 19:04:41 +0100

    lisk deployment

--- onsite-program/submissions/week-3/day-2/George_Chisom/hardhat.config.ts
+++ onsite-program/submissions/week-3/day-2/George_Chisom/hardhat.config.ts
@@ -1,17 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
 require('dotenv').config();
 
+const { WALLET_KEY, LISK_API_KEY, LISK_URL_KEY } = process.env;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.23",
+  solidity: "0.8.30",
   networks: {
-    // for testnet
-    'lisk-sepolia': {
-      url: 'https://rpc.sepolia-api.lisk.com',
-      accounts: [process.env.WALLET_KEY as string],
-      gasPrice: 1000000000,
+    lisk: {
+      url: LISK_URL_KEY,
+      accounts: [WALLET_KEY],
     },
+  },
+  etherscan: {
+    apiKey: LISK_API_KEY as string,
   },
 };
 
