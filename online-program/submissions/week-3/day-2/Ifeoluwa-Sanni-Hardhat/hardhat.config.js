0fbdcc824efd172553685d42cf71bb0424b1a3f4
pheobeayo <pheobeayo@gmail.com>
2025-07-22 22:25:00 +0100

    verify-deployment

--- online-program/submissions/week-3/day-2/Ifeoluwa-Sanni-Hardhat/hardhat.config.js
+++ online-program/submissions/week-3/day-2/Ifeoluwa-Sanni-Hardhat/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const { vars } = require("hardhat/config"); // npx hardhat vars set KEYS
+const {vars} = require("hardhat/config"); // npx hardhat vars set KEYS
 
 const ACCOUNTS = vars.has("PRIVATE_KEY") ? [vars.get("PRIVATE_KEY")] : [];
 const ETHERSCAN_API_KEY = vars.has("ETHERSCAN_API_KEY") ? vars.get("ETHERSCAN_API_KEY") : "";
