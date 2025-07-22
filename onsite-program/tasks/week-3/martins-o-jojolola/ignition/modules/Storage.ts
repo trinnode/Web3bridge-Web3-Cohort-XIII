9def4293152d1a016c45fafbdf1e11adb2125197
Martins-O <jojololamartins686@gmail.com>
2025-07-22 16:40:16 +0100

    Week 3 day 2 class work is done

---
+++ onsite-program/tasks/week-3/martins-o-jojolola/ignition/modules/Storage.ts
@@ -0,0 +1,19 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const JAN_1ST_2030 = 1893456000;
+const ONE_GWEI: bigint = 1_000_000_000n;
+
+const LockModule = buildModule("StorageModule", (m) => {
+
+
+  const lock = m.contract("Storage", [], {
+    
+  });
+
+  return { lock };
+});
+
+export default LockModule;
