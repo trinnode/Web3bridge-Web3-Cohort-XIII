a7e1cc586816d4b700f898129560db1bdc0fc284
wheezard <pelumiemmanuelfadeni@gmail.com>
2025-07-22 13:18:01 -0700

    Add week-3 day-2 task for Pelumi-Emmanuel-Fadeni-storage-contract

---
+++ onsite-program/submissions/week-3/day-2/Pelumi-Emmanuel-Fadeni/ignition/modules/Storage.ts
@@ -0,0 +1,20 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const StorageModule = buildModule("StorageModule", (m) => {
+  const storage = m.contract('Storage');
+  // const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+  // const lock = m.contract("Lock", [unlockTime], {
+  //   value: lockedAmount,
+  // });
+
+  return { storage };
+});
+
+
+export default StorageModule;
