f8a7079735730c595c01d27d31da52cc36466584
0xdothis <olagokesikiru06@gmail.com>
2025-07-22 18:14:54 +0100

    fixe

--- onsite-program/submissions/week-3/day-2/Sikiru Olagoke/contracts/Storage.sol
+++ onsite-program/submissions/week-3/day-2/Sikiru Olagoke/contracts/Storage.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 /**
- * @title Storage
+ * @title Storages
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
