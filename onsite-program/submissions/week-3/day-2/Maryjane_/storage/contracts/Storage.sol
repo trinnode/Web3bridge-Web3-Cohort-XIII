46bd164c51571bf227b667652e2a6db579bc5df4
Ryjen1 <ryjenspark@gmail.com>
2025-07-23 21:33:42 +0100

    Day-3 Assignement

--- onsite-program/submissions/week-3/day-2/Maryjane_/storage/contracts/Storage.sol
+++ onsite-program/submissions/week-3/day-2/Maryjane_/storage/contracts/Storage.sol
@@ -1,15 +1,29 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+// SPDX-License-Identifier: GPL-3.0
 
-contract Greeter {
-   string public text;
+pragma solidity >=0.8.2 <0.9.0;
 
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract Storage {
 
-   function setMessage(string memory _text) public  {
-    text = _text;
-   }
+    uint256 number;
 
-   function getMessage() public view returns(string memory) {
-        return text;
-   }
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieve() public view returns (uint256){
+        return number;
+    }
 }
