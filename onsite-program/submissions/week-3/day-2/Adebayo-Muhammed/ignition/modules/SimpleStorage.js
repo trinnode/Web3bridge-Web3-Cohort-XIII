b00d9c518182b1dd4a8236c5f497bbd53356c3a8
Khemmie-Ray <atokemmy@gmail.com>
2025-07-23 22:44:03 +0100

    remove excess hardhat folders

--- onsite-program/submissions/week-3/day-2/Adebayo-Muhammed/ignition/modules/SimpleStorage.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("SimpleStorageModule", (m) => {
-  const storage = m.contract("SimpleStorage");
-  return { storage };
-});
