616ddb834b7fbe78f62d2d6e485363c7fe491c3b
trinnode <koinated@gmail.com>
2025-07-31 23:14:06 +0100

    ISah-Dauda

--- onsite-program/submissions/week-4/day-4/day4-task-1/Isah-Dauda/ignition/modules/AccessControl.ts
+++ onsite-program/submissions/week-4/day-4/day4-task-1/Isah-Dauda/ignition/modules/AccessControl.ts
@@ -1,9 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const garageAccessModule = buildModule("garageAccessModule", (m) => {
-  const garageAccess = m.contract("garageAccess"); 
+  const garageAccess = m.contract("garageAccess");
 
   return { garageAccess };
 });
 
 export default garageAccessModule;
+
+//Yes
